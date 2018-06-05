ac772aa4d17c13328e5d009a1245156f129d134c
hamopro <39804450+hamopro@users.noreply.github.com>
2018-06-05 02:25:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,125 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+
+
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('** ٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'رابط') {
+    msg.reply('** ``https://discord.gg/pm6Tk8z``**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'الرابط') {
+    msg.reply('** ``https://discord.gg/pm6Tk8z``**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('**وّلّلّّكّكّممّّ نۨــۏڕٺ ۛ ּيۧ مۭــڗ ۛ ּ، ۛ ּمۭــنۨ ۖ ڙمۭــٰا̍نۨ ̨؏ــڼۨــک ۗ :rose::wave:**');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '.') {
+    msg.reply('**يلبى الى ينقط كمل يا حلو **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '..') {
+    msg.reply('**لسا بدك تنقط كمل خلينا نشوف نقطك**');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '...') {
+    msg.reply('**ما اجمل نقطك كمل كمان  **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '....') {
+    msg.reply('**خلاص ما بكمل كمل لوحدك **');
+  }
+});
+
+
+
+client.on('message', msg => {
+  if (msg.content === '.....') {
+    msg.reply('**انت حمار لسا  بتسمع كلامى :joy: **');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === '......') {
+    msg.reply('**خلاص يا ابن الحلال وقف **');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('**ۆعـلُـيَـگـم آلُـسـلُآم ۆرحٍـمةّ آلُلُہ ۆبْرگآتٌہ ، منْۆر. :rose: **');
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'شكرا') {
+    msg.reply('**``آإلعفو قـ♥̨̥̬لبـ♥̨̥̬ي``**');
+  }
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`HaMo BoT,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+  
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
