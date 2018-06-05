a0d279dd9eead28eb66ece9bbffcac86c748e88d
hamopro <39804450+hamopro@users.noreply.github.com>
2018-06-05 06:03:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,17 +26,7 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === 'رابط') {
-    msg.reply('** ``https://discord.gg/pm6Tk8z``**');
-  }
-});
 
-client.on('message', msg => {
-  if (msg.content === 'الرابط') {
-    msg.reply('** ``https://discord.gg/pm6Tk8z``**');
-  }
-});
 
 client.on('message', msg => {
   if (msg.content === 'باك') {
@@ -146,6 +136,32 @@ client.on("message", function(message) {
   };
 });
   
+
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
